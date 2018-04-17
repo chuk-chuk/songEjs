271b4792be85878e11d37d3f3432f33c4236ae2d
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-04-17 17:19:24 +0100

    fetch data from API and send to the view

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const url = 'http://www.songsterr.com/a/ra/songs.json?pattern=Marley';
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-  res.render('index')
-})
+const fetchData = () => {
+  return fetch(url).then((response) => {
+    if(response.ok) {
+      return response.json()
+    } else {
+      console.log('Network request for artists failed with response ' + response.status + ': ' + response.statusText);
+    }
+  });
+}
 
-app.listen(port);
+const prepareDataToRender = (data) => {
+  const listOfRecords = data.map((record) => {
+    return {
+      title:record.title,
+      chords:record.chordsPresent
+    }
+  });
+  return listOfRecords;
+}
+
+app.get('/', (req, res, next) => {
+  fetchData()
+    .then(prepareDataToRender)
+    .then((listOfRecords) => {
+      res.render('index', {
+        records: listOfRecords
+      });
+    });
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.listen(port, () => console.log('Node.js listening on port ' + port));
