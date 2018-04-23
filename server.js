921074238e68d19751aafadcea9158385af4fe0f
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-04-23 21:40:33 +0100

    pass query from the search bar to the view and separate results into separate view

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const ejs = require('ejs');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fetch = require("node-fetch");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const url = 'http://www.songsterr.com/a/ra/songs.json?pattern=Marley';
+const url = 'http://www.songsterr.com/a/ra/songs.json?pattern=';
 
 app.set('view engine', 'ejs');
 
-const fetchData = () => {
-  return fetch(url).then((response) => {
+const fetchData = (query) => {
+  const urlToGet = `${url}${query}`
+  return fetch(urlToGet).then((response) => {
     if(response.ok) {
       return response.json()
     } else {
@@ -29,19 +29,26 @@ const prepareDataToRender = (data) => {
   return listOfRecords;
 }
 
-app.get('/', (req, res, next) => {
-  fetchData()
-    .then(prepareDataToRender)
-    .then((listOfRecords) => {
-      res.render('index', {
-        records: listOfRecords
-      });
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/songs', (req, res) => {
+  const query = req.query.pattern
+  fetchData(query)
+  .then(prepareDataToRender)
+  .then((listOfRecords) => {
+    res.render('partials/results', {
+      records: listOfRecords
     });
+  })
+  .catch((err) => {
+      console.log(err)
+      res.render('error')
+    })
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log('Node.js listening on port ' + port));
